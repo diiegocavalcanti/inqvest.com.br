885ccd52938795cf14f5c022976428c77b8a8ca6
Diego Cavalcanti <diegocavalcanti@outlook.com>
2017-09-08 07:54:29 -0300

    Desenvolvimento de 5 páginas, adição de imagens, atualizações gerais.

---
+++ inc/teste-conhecimento.inc.php
@@ -0,0 +1,88 @@
+<?php include("inc/section-letreiro-slide.inc.php"); ?>
+<section class="section-titulos teste-conhecimentos">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<img src="src/img/titulo-teste-conhecimento-03.svg" class="img-responsive" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-conhecimento-disciplina">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8">
+				<h2>INGLÊS</h2>
+				<p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et odio tempore sapiente, nulla debitis ad voluptatum neque, laudantium libero ipsam possimus qui sint quo eveniet. Minus aliquid dignissimos alias at repellendus obcaecati eum facere eligendi quae, omnis distinctio corporis ea.</p>
+				<h6 class="h6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga pariatur, minima eligendi unde quas recusandae?</h6>
+			</div>
+			<div class="col-sm-4">
+				<div class="container-materia">
+					<div class="row">
+						<div class="col-xs-4">
+							<h3>Português</h3>
+							<h6>Órgão/Banca</h6>
+						</div>
+						<div class="col-xs-3">
+							<p class="coin"><span><img src="src/img/coin.png" alt=""></span>x5</p>
+						</div>
+						<div class="col-xs-5">
+							<p class="text-rv">Restam x Vagas</p>
+							<p class="text-hora">00:00</p>
+							<a href="" class="button-jogar animation">Jogar</a>
+						</div>
+					</div>
+				</div>
+				<div class="container-pontuadores">
+					<img src="src/img/ico-pontuadores-blue.svg" alt="">
+					<h4>MAIORES PONTUADORES</h4>
+				</div>
+				<div class="container-estatisticas">
+					<div class="row">
+						<div class="col-xs-4">
+							<img src="src/img/ico-user.svg" width="20%" alt="" class="img-ico">
+							<h6><span><img src="src/img/ico-down.svg" alt=""></span> 45pts</h6>
+						</div>
+						<div class="col-xs-4">
+							<img src="src/img/ico-user.svg" width="20%" alt="" class="img-ico">
+							<h6><span><img src="src/img/ico-up.svg" alt=""></span> 45pts</h6>
+						</div>
+						<div class="col-xs-4">
+							<img src="src/img/ico-user.svg" width="20%" alt="" class="img-ico">
+							<h6><span><img src="src/img/ico-up.svg" alt=""></span> 45pts</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section-dicas">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<img src="src/img/star-blue-04.svg" class="img-responsive" alt="">
+				<h3>Dica 1</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non rem eum illum saepe amet, beatae, nihil repudiandae voluptatibus similique veniam maxime praesentium, ex nemo quas expedita ratione delectus hic autem.</p>
+			</div>
+			<div class="col-sm-4">
+				<img src="src/img/star-blue-04.svg" class="img-responsive" alt="">
+				<h3>Dica 2</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit adipisci, autem officiis laudantium deleniti, minus ipsam possimus totam culpa impedit magnam rerum consectetur molestias, repellendus veritatis voluptate, nostrum! Asperiores, praesentium?</p>
+			</div>
+			<div class="col-sm-4">
+				<img src="src/img/star-blue-04.svg" class="img-responsive" alt="">
+				<h3>Dica 3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde explicabo sunt quod nulla ratione similique harum doloribus, quisquam facere labore, natus distinctio fugit officiis, iste earum magni pariatur maiores iusto!</p>
+			</div>
+			<div class="col-xs-12">
+				<a href="">JOGAR</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="bg-cinza">
+	<?php include("inc/section-itens-home.inc.php"); ?>
+</div>
--- index.php
+++ index.php
@@ -16,6 +16,21 @@
 			if($_GET["url"] == "home"){
 				include("inc/home.inc.php");
 			}
+			if($_GET["url"] == "teste-conhecimento"){
+				include("inc/teste-conhecimento.inc.php");
+			}
+			if($_GET["url"] == "duelos"){
+				include("inc/duelos.inc.php");
+			}
+			if($_GET["url"] == "desafios"){
+				include("inc/desafios.inc.php");
+			}
+			if($_GET["url"] == "planos"){
+				include("inc/planos.inc.php");
+			}
+			if($_GET["url"] == "noticias"){
+				include("inc/noticias.inc.php");
+			}
 
 
 		} else{
